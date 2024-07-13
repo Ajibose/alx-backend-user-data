b5c4ab7eb5829243a192aaff4d1a6ad7a65e518e
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-13 20:17:44 +0100

    Task7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,17 @@
     Authentication module
 """
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """Basic Autentication"""
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+
+        if re.match(r'Basic ', authorization_header):
+            match = re.fullmatch(r'Basic ([\w=]+)', authorization_header)
+            if match:
+                return match.group(1)
