3d41c88100c7d23cf3a5c2812497f11f8037f03f
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-13 20:01:55 +0100

    Task6 - Basic Authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,7 @@ auth_mechanism = getenv('AUTH_TYPE')
 if auth_mechanism == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-else if auth_mechanism == 'basic_auth':
+elif auth_mechanism == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
