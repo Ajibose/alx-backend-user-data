92723f3496f5dfe9d47c55219f25396bf3472d7a
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-07-28 21:07:34 +0100

    Task1 - add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -32,40 +29,11 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """_summary_
-        """
-        new_user = User(email=email, hashed_password=hashed_password)
-        # add new user and commit to database
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
-
-    def find_user_by(self, **kwargs) -> User:
-        """_summary_
-
-        Returns:
-            User: _description_
-        """
-        if not kwargs:
-            raise InvalidRequestError
-
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if not user:
-            raise NoResultFound
-        return user
-
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """_summary_
-
-        Args:
-            user_id (int): _description_
-        """
-        user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            if not hasattr(user, key):
-                raise ValueError
-            setattr(user, key, value)
-
-        self._session.commit()
-        return None
+    def add_user(self, email, hashed_password):
+        """Adds a user to the database"""
+        u = User()
+        u.email = email
+        u.hashed_password = hashed_password
+        self._session.add(u)
+        self._seission.commit()
+        return u
