64a9f3dea4a3b654927c189a4dc6a37b9c278b8d
Ajibose Ibrahim <ajiboseibrahim12@gmail.com>
2024-08-09 03:43:00 +0100

    Authenticate user class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,7 +16,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> Optional[User]:
+    def register_user(self, email: str, password: str) -> User:
         """Authenticate user"""
         try:
             user = self._db.find_user_by(email=email)
